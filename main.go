0931ec84add3d4ce92c066134df49e581875ca69
Re3v3s <linkin_pong_208@hotmail.com>
2020-07-21 18:09:46 +0700

    okie

--- main.go
+++ main.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"fmt"
+	// Sent Value u need to import template
 	"html/template"
 	_ "log"
 	"net/http"
-	// Sent Value u need to import template
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-// Product is fucked
+//! ResultData is fucked
 // ! when u create struct u need to Comment over them
-type Product struct {
-	Name  string
-	Price int
+type ResultData struct {
+	 Id int
+	 Age int
+	 Fname string
+	 Lname string
 }
 
 func main() {
+
 	// route
 	http.HandleFunc("/", index)
-	// http.HandleFunc("/index", index)
 
 	http.HandleFunc("/home", home)
 
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/save", save)
-
 
 	// runsever with PORT
 	http.ListenAndServe(":8000", nil)
@@ -36,23 +39,53 @@ func index(w http.ResponseWriter, r *http.Request) {
 	//! cant use with ParseFiles
 	// http.ServeFile(w, r, "index.html")
 
+	//! connect
+	var db, err = sql.Open("mysql", "root:@/golang")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+	//! query SQL
+	rows, err := db.Query("select * from users")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	tRes := ResultData{}
+	var results []ResultData
+	//! loop with Next()
+	for rows.Next() {
+		var Id int
+		var Age int
+		var Fname string
+		var Lname string
+
+		err = rows.Scan(&Id, &Fname, &Lname, &Age)
+		tRes.Id = Id
+		tRes.Fname = Fname
+		tRes.Lname = Lname
+		tRes.Age = Age
+		results = append(results,tRes)
+			if err != nil {
+				panic(err)
+			}
+	}
 
 	// sent data
 	var templates = template.Must(template.ParseFiles("index.html"))
-	myProduct := Product{"Milk", 40}
-	templates.ExecuteTemplate(w, "index.html", myProduct)
-
+	templates.Execute(w,results)
+	fmt.Println(results)
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w,r, "login.html")
+	http.ServeFile(w, r, "login.html")
 }
 
 func save(w http.ResponseWriter, r *http.Request) {
 	// receive Request with Request
 	fmt.Println("Method: ", r.Method)
 	r.ParseForm()
-	fmt.Println("Username: " , r.Form["firstname"])
+	fmt.Println("Username: ", r.Form["firstname"])
 	fmt.Println("Password: ", r.Form["lastname"])
 
 }
@@ -61,8 +94,10 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, " this is Home page my friend hey")
 }
 
-
-
+// ! for auto reload
 // use gin for auto reload
 // gin "gin -a 8000 -p 8080 run (filename).go"
 // -a = app port / -p = web app port
+
+// ! for SQL
+//  go get -u github.com/go-sql-driver/mysql
